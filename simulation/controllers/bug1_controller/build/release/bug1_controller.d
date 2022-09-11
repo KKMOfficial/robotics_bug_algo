f16b81277e24b4701eec4137042a1f026582068e
KKMOfficial <koorosh.khavari.m@gmail.com>
2022-09-11 05:08:13 +0430

    final_update

---
+++ simulation/controllers/bug1_controller/build/release/bug1_controller.d
@@ -0,0 +1,7 @@
+build/release/bug1_controller.o: bug1_controller.c \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/robot.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/types.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/nodes.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/motor.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/utils/ansi_codes.h
---
+++ simulation/controllers/bug2_controller/build/release/bug2_controller.d
@@ -0,0 +1,8 @@
+build/release/bug2_controller.o: bug2_controller.c \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/robot.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/types.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/nodes.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/gyro.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/motor.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\apps\0-installable\webots\Webots/include/controller/c/webots/utils/ansi_codes.h
